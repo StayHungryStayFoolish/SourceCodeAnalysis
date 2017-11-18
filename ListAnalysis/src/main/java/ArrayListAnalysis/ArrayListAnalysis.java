e6e2538aa9dafcbb4b5937ccf84412766b01a038
StayHungryStayFoolish <bonismo@hotmail.com>
2017-11-19 00:34:35 +0800

    commit

--- ListAnalysis/src/main/java/ArrayListAnalysis/ArrayListAnalysis.java
+++ ListAnalysis/src/main/java/ArrayListAnalysis/ArrayListAnalysis.java
@@ -33,6 +33,59 @@ public class ArrayListAnalysis {
         elementData = new Object[initialCapacity];
     }
 
+    /* add Method , 默认添加到列表末尾 */
+    public void add(Object o) {
+        // 检查是否需要扩容
+        ensureCapacity();
+        // 将元素添加到末尾
+        elementData[size++] = o;
+    }
+
+    /* add Method at index position */
+    public void add(int index, Object o) {
+        // 检查下标是否越界
+        rangeCheck(index);
+        // 数组扩容与拷贝
+        ensureCapacity();
+        // 复制数组 elementData 从 index 开始，复制到 elementData，从 index+1 开始，复制 index 后边的数组长度（size - index）
+        System.arraycopy(elementData, index, elementData, index + 1, size - index);
+        // 将当前元素复制到索引位置
+        elementData[index] = o;
+        size++;
+    }
+
+    // 删除该索引位置的对象
+    public void remove(int index) {
+        rangeCheck(index);
+        int numMove = size - index - 1;
+        if (numMove > 0) {
+            System.arraycopy(elementData, index + 1, elementData, index, numMove);
+            elementData[--size] = null;
+        } else if (numMove == 0) { // 如果是最后一个索引位置，直接删除元素
+            elementData[--size] = null;
+        }
+    }
+
+    // 数组的扩容与数组拷贝
+    private void ensureCapacity() {
+        if (size == elementData.length) {
+            Object[] newArray = new Object[size * 2 + 1];
+            System.arraycopy(elementData, 0, newArray, 0, elementData.length);
+            elementData = newArray;
+        }
+    }
+
+    // 检测数组下标越界
+    private void rangeCheck(int index) {
+        if (index < 0 || index > size) {
+            try {
+                throw new Exception("ArrayIndexOfBondsException !");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     // 返回 ArrayList 大小
     public int size() {
         return size;
@@ -41,5 +94,11 @@ public class ArrayListAnalysis {
     // 判断当前数组是否为空
     public boolean isEmpty() {
         return size == 0;
+    }
+
+    // 返回当前索引处的值
+    public Object get(int index) {
+        rangeCheck(index);
+        return elementData[index];
     }
 }
